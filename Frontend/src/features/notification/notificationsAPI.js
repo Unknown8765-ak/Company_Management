3c4beb23c93451461f821ff93b3cf60c869ca21d
Amit Maurya <unknownstudent8765@gmail.com>
2026-04-09 18:13:21 +0530

    Update API base URL and notification endpoints

--- Frontend/src/features/notification/notificationsAPI.js
+++ Frontend/src/features/notification/notificationsAPI.js
@@ -1,16 +1,16 @@
 
 
-const API_BASE_URL = "http://localhost:8000/api/v1/notifications"
+const API_BASE_URL = "https://company-management-5yta.onrender.com"
 
 const getNotifications = async () => {
-  const res = await fetch(`${API_BASE_URL}/notification`, {
+  const res = await fetch(`${API_BASE_URL}api/v1/notifications/notification`, {
     credentials: "include"
   });
   return res.json();
 };
 
 const markAsRead = async (id) => {
-  await fetch(`${API_BASE_URL}/mark-read/${id}`, {
+  await fetch(`${API_BASE_URL}api/v1/notifications/mark-read/${id}`, {
     method: "PATCH",
     credentials: "include"
   });
