0c1d6aee511c448a288e6cab85f91e6e60b949e9
Amit Maurya <unknownstudent8765@gmail.com>
2026-04-10 21:21:42 +0530

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const API_URL ="http://localhost:8000";
+  const API_URL = import.meta.env.VITE_API_URL;
 
   const handleLogout = async () => {
     try {
